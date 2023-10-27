44d8e7299abd536474321196b77a0ec292a90178
Jan Frommann <jf.frommann@gmail.com>
2023-10-27 16:03:10 +0100

    x

--- Home.py
+++ Home.py
@@ -28,13 +28,24 @@ def main():
              
     ### Tools
              
+    This experience included one interactive data tool and two distinct LLM chatbot pages. The "Iris Flower Experience" and "Talk with Judea" are introduced in the relevant chapters.
+             
+    You can always access our ChatGPT implementation to deepen your learning or when you feel stuck. Just keep in mind that for ChatGPT, we only allow preset prompts to avoid cheating. Also, LLMs make mistakes, watch out!
+             
     ### Pages & Chapters
+             
+    Each Chapter links to a Notion page with the chapter content and links to any further material. You can always go back to this page and use the tools provided here. We recommend opening the Notion pages in seperate tabs.
              
     ### Concept & Review Blocks
              
     Most chapters include Concepts Blocks, wich briefly summarize key concepts that have been introduced in the material. Use them to refresh your memory or make sure you understood the concept. Also, at the end of most pages you find a Review Block. Make sure you understood the concepts listed there before moving on.
              
     ### Tasks & Assessments
+             
+    Tasks are indicated in the Chapter pages. Please follow along carefully and stick to the instructions. The Springfield Assignment and the Final Quiz are used to measure your learning progress at the end of this experience.
+    
+    ### Start with Chapter 1. Have fun! 🥳
+             
     """)
 
 if __name__ == "__main__":
