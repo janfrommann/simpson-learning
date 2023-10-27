e8efa63247bb8d5eb917ff6d83a515b450e5cabc
Jan Frommann <jf.frommann@gmail.com>
2023-10-27 15:55:56 +0100

    x

---
+++ pages/10_[Test]_Final_Quiz.py
@@ -0,0 +1,12 @@
+import openai
+import streamlit as st
+from streamlit_feedback import streamlit_feedback
+import trubrics
+
+
+st.title("📝 [Test] Final Quiz")
+
+"""
+For this Lesson, please go to the following Notion [Page](https://jf2.notion.site/Chapter-6-Quiz-69030e53eeab44deb219c7e63d3cf857?pvs=4) in another tab. You can always go back here to use the ineractive tools. Have fun!
+"""
+
--- pages/9_Chapter_5_[Assignment]:_Cracking_the_Springfield_Case.py
+++ pages/9_Chapter_5_[Assignment]:_Cracking_the_Springfield_Case.py
@@ -7,6 +7,6 @@ import trubrics
 st.title("📝 [Test] Final Quiz")
 
 """
-For this Lesson, please go to the following Notion [Page](https://jf2.notion.site/Chapter-6-Quiz-69030e53eeab44deb219c7e63d3cf857?pvs=4) in another tab. You can always go back here to use the ineractive tools. Have fun!
+For this Lesson, please go to the following Notion [Page](https://jf2.notion.site/Chapter-5-Assignment-Cracking-the-Springfield-Case-with-HomerGPT-ac4a5c2ee4634e2dab1d824169fb5332?pvs=4) in another tab. You can always go back here to use the ineractive tools. Have fun!
 """
 
